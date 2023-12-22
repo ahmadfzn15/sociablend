3c6ce0d3127f332ec7742f712d2e6e24907e1536
ahmad fauzan <ahmadfzn2005@gmail.com>
2023-12-22 16:04:39 +0700

    new commit

--- app/(pages)/explore/page.tsx
+++ app/(pages)/explore/page.tsx
@@ -1,17 +1,8 @@
-"use client";
-
-import { AuthContext } from "@/app/context";
-import { useContext } from "react";
-
 export default function Explore() {
-  const { user } = useContext(AuthContext);
-
   return (
     <>
       <div className="p-5 w-full h-full">
-        <div className="w-full h-full bg-slate-900/80 rounded-lg">
-          {user && user.email}
-        </div>
+        <div className="w-full h-full bg-slate-900/80 rounded-lg"></div>
       </div>
     </>
   );
